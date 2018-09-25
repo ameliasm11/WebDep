83706890e42e9bfd91ef1e54aea6c3b022b88349
Lufianti <43260460+Lufianti@users.noreply.github.com>
2018-09-25 16:14:14 +0700

    update futsal.blade.php

--- resources/views/SuperAdmin/Pages/Product/Futsal/futsal.blade.php
+++ resources/views/SuperAdmin/Pages/Product/Futsal/futsal.blade.php
@@ -16,7 +16,7 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            <strong class="card-title">Futsal Jadwal</strong>
+                            <strong class="card-title">List Futsal</strong>
                         </div>
                     <div class="card-body">
                       <div class="row form-group">
