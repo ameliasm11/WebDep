ae1fd1b79d1cf891e3a90f6e0c65f71f9dfd9346
Lufianti <43260460+Lufianti@users.noreply.github.com>
2018-10-12 18:44:16 +0700

    update()

--- resources/views/SuperAdmin/Pages/Product/Futsal/newTempat.blade.php
+++ resources/views/SuperAdmin/Pages/Product/Futsal/newTempat.blade.php
@@ -31,13 +31,65 @@
                             <div class="col-12 col-md-9"><input type="file" class="form-control" id="gambar" name="gambar">
                           </div>
                         </div>
-                        <div class="row form-group">
-                          <div class="col col-md-3"><label for="text-input" class="form-control-label">Jam Buka</label></div>
-                          <div class="col-12 col-md-9"><input type="time" id="jam_buka" name="jam_buka" placeholder="Jam Buka" class="form-control"></div>
+                        <div class="col col-md-1"><label for="select" class=" form-control-label">Jam Buka</label></div>
+                        <div class="col-12 col-md-3">
+                          <select onchange="document.location.href=this.options[this.selectedIndex].value;" name="jam_buka" id="jam_buka" class="form-control">
+                            <option value="0" disabled selected>Pilih</option>
+                            <option value="1">01.00</option>
+                            <option value="2">02.00</option>
+                            <option value="3">03.00</option>
+                            <option value="4">04.00</option>
+                            <option value="5">05.00</option>
+                            <option value="6">06.00</option>
+                            <option value="7">07.00</option>
+                            <option value="8">08.00</option>
+                            <option value="9">09.00</option>
+                            <option value="10">10.00</option>
+                            <option value="11">11.00</option>
+                            <option value="12">12.00</option>
+                            <option value="13">13.00</option>
+                            <option value="14">14.00</option>
+                            <option value="15">15.00</option>
+                            <option value="16">16.00</option>
+                            <option value="17">17.00</option>
+                            <option value="18">18.00</option>
+                            <option value="19">19.00</option>
+                            <option value="20">20.00</option>
+                            <option value="21">21.00</option>
+                            <option value="22">22.00</option>
+                            <option value="23">23.00</option>
+                            <option value="24">24.00</option>
+                          </select>
                         </div>
-                        <div class="row form-group">
-                          <div class="col col-md-3"><label for="text-input" class="form-control-label">Jam Tutup</label></div>
-                          <div class="col-12 col-md-9"><input type="time" id="jam_tutup" name="jam_tutup" placeholder="Jam Buka" class="form-control"></div>
+                        <div class="col col-md-1"><label for="select" class=" form-control-label">Jam Tutup</label></div>
+                        <div class="col-12 col-md-3">
+                          <select onchange="document.location.href=this.options[this.selectedIndex].value;" name="jam_tutup" id="jam_tutup" class="form-control">
+                            <option value="0" disabled selected>Pilih</option>
+                            <option value="1">01.00</option>
+                            <option value="2">02.00</option>
+                            <option value="3">03.00</option>
+                            <option value="4">04.00</option>
+                            <option value="5">05.00</option>
+                            <option value="6">06.00</option>
+                            <option value="7">07.00</option>
+                            <option value="8">08.00</option>
+                            <option value="9">09.00</option>
+                            <option value="10">10.00</option>
+                            <option value="11">11.00</option>
+                            <option value="12">12.00</option>
+                            <option value="13">13.00</option>
+                            <option value="14">14.00</option>
+                            <option value="15">15.00</option>
+                            <option value="16">16.00</option>
+                            <option value="17">17.00</option>
+                            <option value="18">18.00</option>
+                            <option value="19">19.00</option>
+                            <option value="20">20.00</option>
+                            <option value="21">21.00</option>
+                            <option value="22">22.00</option>
+                            <option value="23">23.00</option>
+                            <option value="24">24.00</option>
+                          </select>
                         </div>
                         <div class="row form-group">
                           <div class="col col-md-3"><label for="select" class=" form-control-label">Partner</label></div>
